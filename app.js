5f0b3b9e413d176357dc798b83fd992f447a7cce
Filip Maj <fmaj@slack-corp.com>
2022-01-26 17:35:02 -0500

    latest updates

--- app.js
+++ app.js
@@ -2,16 +2,13 @@ const { App } = require('@slack/bolt');
 const fs = require('fs');
 
 // Initializes your app with your bot token and signing secret
-// // Initializes your app in socket mode with your app token and signing secret
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   // logLevel: 'debug',
   manifest: JSON.parse(fs.readFileSync('manifest.json', 'utf-8')),
-  // /*
   socketMode: true, // add this
   appToken: process.env.SLACK_APP_TOKEN // add this
-  // */
 });
 // Listens to incoming messages that contain "hello"
 app.message('hello', async ({ message, say }) => {
