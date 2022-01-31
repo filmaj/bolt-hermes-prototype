3520f1ab54adbeec00a16d4baca0baeda44a4926
Filip Maj <fmaj@slack-corp.com>
2022-01-31 16:54:25 -0500

    triggers command should return empty array

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 // Initializes your app with your bot token and signing secret
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  // signingSecret: process.env.SLACK_SIGNING_SECRET,
   // logLevel: 'debug',
   manifest: JSON.parse(fs.readFileSync('manifest.json', 'utf-8')),
   socketMode: true, // add this
